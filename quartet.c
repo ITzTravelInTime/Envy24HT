0ec0b051b24e162e0cde1512df957b0bce5e5688
ITzTravelInTime <pietrocaruso97@yahoo.it>
2023-11-27 03:51:10 +0100

    Preliminary work to support the quartet infrasonic + slight adjustments for multistream playback (still not working)

---
+++ quartet.c
@@ -0,0 +1,11 @@
+/*
+ *  quartet.c
+ *  Envy24HT_64bit_Driver
+ *
+ *  Created by ITzTravelInTime on 11/26/23.
+ *  Copyright 2023 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "quartet.h"
+
